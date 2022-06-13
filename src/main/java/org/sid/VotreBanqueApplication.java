2811408bb3426cb5f5dbb2163bda1f81c4961ab4
massamba08 <samndodia@gmail.com>
2022-06-13 13:47:17 +0200

    couche dao

--- src/main/java/org/sid/VotreBanqueApplication.java
+++ src/main/java/org/sid/VotreBanqueApplication.java
@@ -2,11 +2,14 @@ package org.sid;
 
 import java.util.Date;
 
-
+import org.sid.dao.ClientRepository;
+import org.sid.dao.CompteRepository;
+import org.sid.dao.OperationRepository;
 import org.sid.entites.Client;
 import org.sid.entites.Compte;
 import org.sid.entites.CompteCourant;
 import org.sid.entites.CompteEpargne;
+import org.sid.entites.Operation;
 import org.sid.entites.Retrait;
 import org.sid.entites.Versement;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,27 +23,43 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class VotreBanqueApplication implements CommandLineRunner {
 	
+	@Autowired
+	private ClientRepository clientRepository;
+	@Autowired
+	private CompteRepository compteRepository;
+	@Autowired
+	private OperationRepository operationRepository;
 	public static void main(String[] args) {
-                SpringApplication.run(VotreBanqueApplication.class, args);
+         SpringApplication.run(VotreBanqueApplication.class, args);
 		
 		
 	}
+
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
+		
+		Client c1 = clientRepository.save(new Client("Massamba", "massamba@gmail.com"));
+		Client c2 = clientRepository.save(new Client("Bass", "bass@gmail.com"));
+		Client c3 = clientRepository.save(new Client("Omar", "omara@gmail.com"));
+		Client c4 = clientRepository.save(new Client("Khadija", "khadija@gmail.com"));
+		
+		Compte cp1 = compteRepository.save(new CompteCourant("C1", new Date(), 90000, c1, 6000));
+		Compte cp2 = compteRepository.save(new CompteEpargne("C2", new Date(), 6000, c2, 5.5));
+		
+		operationRepository.save(new Versement(new Date(), 9000, cp1));
+		operationRepository.save(new Versement(new Date(), 6000, cp1));
+		operationRepository.save(new Versement(new Date(), 2300, cp1));
+		operationRepository.save(new Retrait(new Date(), 9000, cp1));
+		
+		operationRepository.save(new Versement(new Date(), 2300, cp2));
+		operationRepository.save(new Versement(new Date(), 400, cp2));
+		operationRepository.save(new Versement(new Date(), 2300, cp2));
+		operationRepository.save(new Retrait(new Date(), 3000, cp2));
+		
+	}
 		
 	}
 
-	/*
-	 * @Override public void run(String... args) throws Exception { Client c1 =
-	 * clientRepository.save(new Client(null, "Hassan", "hassan@gmail.com", null));
-	 * Client c2 = clientRepository.save(new Client(null, "Ouly", "ouly@gmail.com",
-	 * null));
-	 * 
-	 * 
-	 * 
-	 * 
-	 * }
-	 */
+	
 
-}
+
